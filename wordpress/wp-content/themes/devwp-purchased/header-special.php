fb67da1e79d5a5f0bcca2e5132b3c8214c0da037
Jim Gregor <jimmyg0801@gmail.com>
2019-04-26 05:43:45 -0400

    This is now a working menu (navbar) test page in the purchased theme.

--- wordpress/wp-content/themes/devwp-purchased/header-special.php
+++ wordpress/wp-content/themes/devwp-purchased/header-special.php
@@ -28,123 +28,92 @@
 	</head>
 
 <body <?php body_class(); ?>>
-<div id = "page" class = "site">
+
+	<div id = "page" class = "site">
 	<a class = "skip-link screen-reader-text" href = "#content">
 		<?php esc_html_e( 'Skip to content', 'devwp' ); ?>
 	</a>
-	
-	<header id = "masthead" class = "site-header <?php echo devwp_has_header(); ?>">
-		<nav id = "menu" class = "navbar navbar-expand-lg navbar-dark">
 
-			<div class = "container">
-				<div class = "site-branding navbar-brand">
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h3><?php echo $containerType ?></h3>
-				</div><!-- .site-branding -->
+	<?php get_template_part( 'template-parts/header', 'navbar' ); ?>
 
-					<button class = "navbar-toggler navbar-toggler-right" type = "button" data-toggle = "collapse"
-				        data-target = "#bs4navbar" aria-controls = "bs4navbar" aria-expanded = "false"
-				        aria-label = "Toggle navigation">
-						<span class = "navbar-toggler-icon"></span>
-					</button>
-				
-				<?php
-					wp_nav_menu( [
-						'menu'            => 'primary',
-						'theme_location'  => 'primary',
-						'depth'           => 2,
-						'container'       => 'div',
-						'container_id'    => 'bs4navbar',
-						'container_class' => 'collapse navbar-collapse',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-						'walker'          => new bs4navwalker()
-					] );
-				?>
-			</div><!-- .container -->
-		</nav>
+	<?php if ( is_front_page() && ! is_home() ) :
+		// https://developer.wordpress.org/reference/functions/has_custom_header/
+		// https://developer.wordpress.org/reference/functions/has_header_video/
+		if ( has_header_video() ) : ?>
+			<?php // https://youtu.be/pEJ2IzYOx5c
+			//  https://youtu.be/fxIlVbvOHyY
+			if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) : ?>
+				<div class = "mt-6">
+			<?php endif;
+			// https://developer.wordpress.org/reference/functions/the_custom_header_markup/
+			the_custom_header_markup();
+			
+			if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) : ?>
+				</div>
+			<?php endif;
 
-	</header>
-	<!-- #masthead -->
-
-<?php
-if ( is_front_page() && ! is_home() ) :
-// https://developer.wordpress.org/reference/functions/has_custom_header/
-// https://developer.wordpress.org/reference/functions/has_header_video/
-	if ( has_header_video() ) : ?>
-		<?php // https://youtu.be/pEJ2IzYOx5c
-		//  https://youtu.be/fxIlVbvOHyY
-		if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) : ?>
-			<div class = "mt-6">
-		<?php endif;
-// https://developer.wordpress.org/reference/functions/the_custom_header_markup/
-		the_custom_header_markup();
-		
-		if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) : ?>
+		// https://developer.wordpress.org/reference/functions/has_header_image/
+		elseif ( has_header_image() ) : ?>
+			
+			<div class = "header-image d-flex justify-content-center align-items-center <?php
+				if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) {
+					echo 'mt-6';
+				} ?>">
+				<?php devwp_header_text(); ?>
+				<img src = "<?php header_image(); ?>" width = "<?php echo esc_attr( get_custom_header() -> width ); ?>"
+				     alt = "<?php bloginfo( 'name' ); ?>">
 			</div>
-		<?php endif;
-
-// https://developer.wordpress.org/reference/functions/has_header_image/
-	elseif ( has_header_image() ) : ?>
 		
-		<div class = "header-image d-flex justify-content-center align-items-center <?php
-			if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) {
-				echo 'mt-6';
+		<?php endif;
+		
+		$ffesb = is_active_sidebar( 'front-full-edge' );
+		$ffcsb = is_active_sidebar( 'front-full-centered' );
+		if ( $ffesb ) : ?>
+			<div
+				class = "d-flex justify-content-center <?php if ( 'fixed-top' == $navbarType && ! has_custom_header() ) {
+					echo 'mt-6';
+				} else {echo 'mt-1';} ?>">
+				<?php dynamic_sidebar( 'front-full-edge' ); ?>
+			</div>
+		
+		<?php endif; ?>
+		
+		<div class = "fp <?php echo esc_attr( $containerType );
+			if ( 'fixed-top' == $navbarType && ! has_custom_header() ) {
+				echo ' mt-7';
+			} else {
+				echo ' mt-2';
 			} ?>">
-			<?php devwp_header_text(); ?>
-			<img src = "<?php header_image(); ?>" width = "<?php echo esc_attr( get_custom_header() -> width ); ?>"
-			     alt = "<?php bloginfo( 'name' ); ?>">
-		</div>
-	
 	<?php endif;
-	
-	$ffesb = is_active_sidebar( 'front-full-edge' );
-	$ffcsb = is_active_sidebar( 'front-full-centered' );
-	if ( $ffesb ) : ?>
-		<div
-			class = "d-flex justify-content-center <?php if ( 'fixed-top' == $navbarType && ! has_custom_header() ) {
-				echo 'mt-6';
-			} else {echo 'mt-1';} ?>">
-			<?php dynamic_sidebar( 'front-full-edge' ); ?>
-		</div>
-	
-	<?php endif; ?>
-	
-	<div class = "fp <?php echo esc_attr( $containerType );
-		if ( 'fixed-top' == $navbarType && ! has_custom_header() ) {
+
+	if ( is_home() ) :
+		if ( is_active_sidebar( 'home-full-edge' ) ) : ?>
+
+			<div
+				class = "header-image d-flex justify-content-center align-items-center <?php if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) {
+					echo 'mt-6';
+				} ?>">
+				<?php devwp_header_blog();
+					dynamic_sidebar( 'home-full-edge' ); ?>
+			</div>
+			
+			<div class = "hp1 <?php echo esc_attr( $containerType ); ?>">
+
+		<?php else : ?>
+			
+			<div class = "hp2 <?php echo esc_attr( $containerType );
+				if ( 'fixed-top' == $navbarType ) {
+					echo ' mt-7';
+				} ?>">
+
+		<?php endif;
+	endif;
+
+	if ( ! is_front_page() && ! is_home() ) : ?>
+		<div class = "<?php echo esc_attr( $containerType ); ?>">
+		<div id = "content" class = "site-content row <?php if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) {
 			echo ' mt-7';
 		} else {
-			echo ' mt-2';
-		} ?>">
-<?php endif;
-
-if ( is_home() ) :
-if ( is_active_sidebar( 'home-full-edge' ) ) : ?>
-	<div
-		class = "header-image d-flex justify-content-center align-items-center <?php if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) {
-			echo 'mt-6';
-		} ?>">
-		<?php devwp_header_blog();
-			dynamic_sidebar( 'home-full-edge' ); ?>
-	</div>
-	
-	<div class = "hp1 <?php echo esc_attr( $containerType ); ?>">
-	
-	<?php else : ?>
-	
-	<div class = "hp2 <?php echo esc_attr( $containerType );
-		if ( 'fixed-top' == $navbarType ) {
-			echo ' mt-7';
-		} ?>">
-
-<?php endif;
-endif;
-	
-	if ( ! is_front_page() && ! is_home() ) : ?>
-	<div class = "<?php echo esc_attr( $containerType ); ?>">
-	<div id = "content" class = "site-content row <?php if ( 'fixed-top' == $navbarType || 'offcanvas' == $navbarType ) {
-		echo ' mt-7';
-	} else {
-		echo 'mt-3';
-	} ?> HEADER_END">
+			echo 'mt-3';
+		} ?> HEADER_END">
 	<?php endif;
